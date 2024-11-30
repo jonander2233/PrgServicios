8d9092af03ecd2500739e927c145c01b491af313
Jon Ander <jonander2233@gmail.com>
2024-11-30 18:48:03 +0100

    wip

---
+++ ejercicios/paralelismo02/ej01/__init__.py
@@ -0,0 +1,3 @@
+from ejercicios.paralelismo02.ej01.ej01 import MiScraping
+
+hilo = MiScraping("")
--- ejercicios/paralelismo02/ej01/ej01.py
+++ ejercicios/paralelismo02/ej01/ej01.py
@@ -4,16 +4,16 @@ from bs4 import BeautifulSoup
 import re
 
 class MiScraping(threading.Thread):
-
     def __init__(self,url):
         threading.Thread.__init__(self)
         self.url = url
         self.links =[]
     def run(self):
-        # self.url coge el "url" que se le pase al constructor a la hora de llamar al metodo
         response = requests.get(self.url)
-
-
+        soup = BeautifulSoup(response.text,"html.parser")
+        self.links = [a['href'] for a in soup.find_all('a', href=True) if re.match(r'http[s]?://', a['href'])]
+    def get_links(self):
+        return self.links
 
 
 
