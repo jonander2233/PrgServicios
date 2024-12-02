819bab2e5cd4e7228636feadaf82750f7a188fad
Jon Ander <jonander2233@gmail.com>
2024-12-02 13:58:51 +0100

    wip

--- ejercicios/paralelismo02/ej01/__init__.py
+++ ejercicios/paralelismo02/ej01/__init__.py
@@ -1,3 +1,7 @@
 from ejercicios.paralelismo02.ej01.ej01 import MiScraping
 
-hilo = MiScraping("")
+hilo = MiScraping("http://localhost/mi_web")
+hilo.start()
+hilo.join()
+links = hilo.get_links()
+print(links)
